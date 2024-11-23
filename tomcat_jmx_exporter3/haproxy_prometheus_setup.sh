22f5bcdfbf5f2f01b404c544d24ad9d89f32a876
Guilherme Maas <guilherme.maas@gmail.com>
2024-11-23 16:35:17 -0300

    .xp

--- tomcat_jmx_exporter3/haproxy_prometheus_setup.sh
+++ tomcat_jmx_exporter3/haproxy_prometheus_setup.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-
-# Registrar saída em log
 mkdir -p /var/log/haproxy_exporter && sudo sh -c 'echo "Iniciando script" > /var/log/haproxy_exporter/haproxy_exporter_install.log'
 set -e
 
-# Atualizar pacotes do sistema
 apt-get update -y
 
-# Instalar HAProxy
 apt-get install -y haproxy
 
-# Configurar HAProxy para expor estatísticas
 tee -a /etc/haproxy/haproxy.cfg > /dev/null <<EOL
 
 listen stats
@@ -21,18 +16,14 @@ listen stats
     stats refresh 10s
 EOL
 
-# Reiniciar HAProxy para aplicar as alterações
 systemctl restart haproxy
 
-# Baixar e instalar o HAProxy Exporter para Prometheus
 wget https://github.com/prometheus/haproxy_exporter/releases/download/v0.13.0/haproxy_exporter-0.13.0.linux-amd64.tar.gz
 tar xzf haproxy_exporter-0.13.0.linux-amd64.tar.gz
 mv haproxy_exporter-0.13.0.linux-amd64/haproxy_exporter /usr/local/bin/
 
-# Remover arquivos desnecessários
 rm -rf haproxy_exporter-0.13.0.linux-amd64.tar.gz haproxy_exporter-0.13.0.linux-amd64
 
-# Criar um serviço systemd para o HAProxy Exporter
 tee /etc/systemd/system/haproxy_exporter.service > /dev/null <<EOL
 [Unit]
 Description=Prometheus HAProxy Exporter
@@ -48,10 +39,8 @@ Restart=always
 WantedBy=multi-user.target
 EOL
 
-# Carregar e iniciar o serviço do HAProxy Exporter
 systemctl daemon-reload
 systemctl enable haproxy_exporter
 systemctl start haproxy_exporter
 
-# Registrar saída em log
 sudo sh -c 'echo "HAProxy e Prometheus Exporter instalados e rodando com sucesso." >> /var/log/haproxy_exporter/haproxy_exporter_install.log'
